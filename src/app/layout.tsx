73f92d61a869cce6fb13fe8e2fb0cbcf98b8b232
Pratham271 <chauhanpratham22@gmail.com>
2024-05-24 18:18:26 +0530

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AI } from "../actions/chat";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <AI>
+         
           {children}
-          </AI>
+          
       </body>
     </html>
   );
