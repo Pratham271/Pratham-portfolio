a0c1b77d43f7d098ce30da2f24265bfc3f814c1f
Pratham271 <chauhanpratham22@gmail.com>
2024-05-24 18:09:17 +0530

    updated package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from '@vercel/analytics/react';
+
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 import { AI } from "@/actions/chat";
@@ -23,7 +23,6 @@ export default function RootLayout({
       <body className={inter.className}>
           <AI>
           {children}
-          <Analytics />
           <SpeedInsights />
           </AI>
       </body>
