e4181806962921392206abbddbae9c3c4bc79e15
Pratham271 <chauhanpratham22@gmail.com>
2024-05-24 17:22:12 +0530

    added the pinecone db vector store

---
+++ src/lib/pineconedb.ts
@@ -0,0 +1,25 @@
+import { CohereEmbeddings } from "@langchain/cohere";
+import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeStore } from "@langchain/pinecone";
+
+
+export async function getVectorStore(){
+    const embeddings = new CohereEmbeddings({
+        apiKey: process.env.COHERE_API_KEY, // In Node.js defaults to process.env.COHERE_API_KEY
+        batchSize: 96, // Default value if omitted is 48. Max value is 96
+    });
+    const pineconeClient = new Pinecone({
+        apiKey: process.env.PINECONE_API_KEY!
+    });
+
+    const index = pineconeClient.Index(process.env.PINECONE_INDEX_NAME!);
+
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+        pineconeIndex: index,
+        textKey: 'text',
+      });
+    
+    return vectorStore
+}
+
+
