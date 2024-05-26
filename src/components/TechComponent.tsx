a31bbe33864d63895206ff72a4157bb8b49a8b60
Pratham271 <chauhanpratham22@gmail.com>
2024-05-26 18:08:53 +0530

    updated the ui

--- src/components/TechComponent.tsx
+++ src/components/TechComponent.tsx
@@ -6,35 +6,29 @@ import NavBar from '@/components/NavBar';
 import { technologies } from '@/constants';
 import BallCanvas from '@/components/ui/BallCanvas';
 import { cn } from '@/utils/cn';
+import { Boxes } from './ui/background-boxes';
 const TechComponent = () => {
   return (
-    <div className='h-[90rem] bg-black/[0.96]'>
-      <div className="h-[21rem] lg:h-[30rem] w-full rounded-md flex flex-col md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight
-        className="-top-20 left-0 lg:left-96 lg:-top-20"
-        fill="white"
-      />
-      <div className='absolute top-0 w-full lg:w-fit z-50'>
-        <NavBar/>
-      </div>
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Tech Stack
-        </h1>
-      
-      </div>
-    </div>
-    <div>
-    <div className="flex flex-row flex-wrap absolute top-44  md:static justify-center gap-10">
-    {technologies.map((technology, index) => (
-  <div className="w-28 h-28" key={technology.name}>
-      <BallCanvas imgUrl={technology.icon.src}/>
-  </div>
-))}
+   
+      <div className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+      <div className="absolute inset-0 w-full h-full bg-slate-900 z-10 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+      <Boxes />
+      <div className='absolute top-0 z-20 w-full'>
+       <NavBar/>
+       </div>
+      <h1 className={cn("text-4xl text-white z-10 md:text-6xl font-bold text-center absolute top-10 mt-10")}>
+       Tech Stack
+      </h1>
+        <div className="flex flex-row flex-wrap absolute top-44  md:static justify-center gap-10 z-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name} >
+              <BallCanvas imgUrl={technology.icon.src}/>
+          </div>
+        ))}
 
-</div>
-</div>
-    </div>
+      </div>
+      </div>
+   
   )
 }
 
