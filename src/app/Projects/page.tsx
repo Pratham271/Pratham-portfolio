8fcf813ab9255b800195936b42e8a2e174e803bc
Pratham Chauhan <83640434+Pratham271@users.noreply.github.com>
2024-06-22 13:27:20 +0530

    Update page.tsx

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-  <div className='bg-black'>
+  <div className="bg-black">
       <NavBar/>
       <div className="min-h-screen relative w-full bg-black mt-0 overflow-scroll rounded-md">
       
@@ -25,9 +25,9 @@ const Projects = () => {
       <h1 className="md:text-7xl mt-4 text-3xl lg:text-6xl font-bold text-center text-white  z-20">
         Projects
       </h1>
-      <div className=' z-20'>
-            <div className='w-full flex justify-center'>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 mb-10'>
+      <div className="z-20">
+            <div className="w-full flex justify-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 mb-10">
                        
                             {projects.map((project,index)=> (
                                 <ProjectCard key={index} 
