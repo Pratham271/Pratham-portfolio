7473d1c810d424abf58f53b3e18e28ce39f49af4
Pratham156 <pratham.chauhan@leapx.ai>
2024-07-17 06:32:59 +0530

    updated navbar again

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -25,7 +25,7 @@ const NavBar = () => {
             <div className="lg:hidden flex flex-1 justify-start items-center w-full z-50">
               <Hamburger onClick={() => setToggle(!toggle)}/>
               <div className={`${!toggle ? 'hidden' : 'flex'} p-6 bg-neutral-900  absolute top-10 left-0 lg:mx-4 my-2 w-full lg:w-96 z-50 rounded-xl`}>
-                <ul className='list-none flex justify-center items-center text-center mx-auto flex-col gap-4 w-full hover:bg-wh'>
+                <ul className="list-none flex justify-center items-center text-center mx-auto flex-col gap-4 w-full hover:bg-wh">
                   {navLinks.map((link)=> (
                     <Link href={link.id} key={link.id}><li onClick={() => setToggle(!toggle)} className='cursor-pointer w-96 p-2 hover:bg-neutral-700'>{link.title}</li></Link>
                   ))}
