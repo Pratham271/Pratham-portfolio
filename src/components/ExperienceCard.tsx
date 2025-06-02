6ce41fe5fdeb3e9ea2144b85e1f1d06f5247bf6d
Pratham271 <pratham19csu221@ncuindia.edu>
2025-06-02 15:05:27 +0530

    resolved the missing key bug

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -14,7 +14,7 @@ const ExperienceCard: React.FC<{ experience: Experience }> = ({ experience }) =>
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="text-sm font-normal leading-none text-gray-400 ">{experience.date}</time>
         <h3 className="text-lg font-semibold text-white mt-1">{experience.title} ({experience.company_name})</h3>
-        <div className="mb-4 text-base font-normal text-gray-400 mt-3 max-w-2xl lg:max-w-7xl">
+        <ul className="mb-4 text-base font-normal text-gray-400 mt-3 max-w-2xl lg:max-w-7xl">
           {
             experience.points.map((point,index)=> (
             //  <span key={index}>
@@ -23,18 +23,18 @@ const ExperienceCard: React.FC<{ experience: Experience }> = ({ experience }) =>
             //   <span className="mt-1.5"></span>
               
             //  </span>
-            <ul>
+            // <ul>
               <li 
               key={index} 
               className="flex items-start gap-3"
             >
               <span className="text-gray-500 font-mono text-lg leading-none mt-1">-</span>
-              <span>{point}</span>
+              <span key={index}>{point}</span>
             </li>
-            </ul>
+            // </ul>
             ))
           }
-          </div>
+          </ul>
     </li>
     
     
